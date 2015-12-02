c1382f8541604e6162b635efc407fa37186d803a
demkacode <demkacode@gmail.com>
2015-12-02 13:35:45 +0200

    stat the project

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+// Обязательная обёртка
+module.exports = function(grunt) {
+
+    // Задачи
+    grunt.initConfig({
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'css/style.css': 'sass/main.scss'
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1', 'IE 9']
+            },
+            style: {
+                src: 'css/style.css'
+            },
+        },
+
+
+        watch: {
+            options: {
+              livereload: true,
+            },
+            css: {
+                files: ['sass/main.scss'],
+                tasks: ['sass', 'autoprefixer'],
+                options: {
+                  spawn: false,
+                   livereload: true,
+                },
+            },
+        }
+    });
+
+
+    // Загрузка плагинов, установленных с помощью npm install
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-spritesmith');
+
+    // Задача по умолчанию
+    grunt.registerTask('default', ['sass', 'autoprefixer']);
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+# Pink-Mobile_new
